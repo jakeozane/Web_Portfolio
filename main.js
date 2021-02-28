0cf0b3145f03e381cb358fee26a5e618ae70a186
Jacob Morrow <60331042+Morrow98J@users.noreply.github.com>
2021-02-27 19:07:20 -0600

    fix bugs, add main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', bindEmail);
+
+function bindEmail() {
+    document.getElementById('emailSubmit').addEventListener('click', function(event) {
+    let req = new XMLHttpRequest();
+    let payload = {
+        email: null
+    };
+    payload.email = document.getElementById('email').value;
+    req.open('POST', 'http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function(){
+        if (req.status >= 200 && req.status < 400) {
+            var response = JSON.parse(req.responseText);
+            document.getElementById('email').textContent = response.email;
+            console.log(JSON.parse(req.responseText));
+        } else {
+            console.log('Error in network request: ' + req.status);
+        }
+    });
+    req.send(JSON.stringify(payload));
+    event.preventDefault();
+    });
+}
